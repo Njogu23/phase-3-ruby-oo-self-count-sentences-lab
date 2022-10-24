d9cb1065b3eae6f631d938f8a3c3ca887ece0a4d
George Njogu <sbeveme@gmail.com>
2022-10-24 16:11:50 +0300

    assignment done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+      if self.scan(/\W/).grep(/\S/).count == 0
+        self.split(/\W/).count
+      else
+    self.scan(/\W/).grep(/\S/).concat(self.split.grep(/\w/)).count
+      end
   end
 end
